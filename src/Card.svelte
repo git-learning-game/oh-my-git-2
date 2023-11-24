84e1b3a16c3ef37eb7daa896de851c10b9416604
bleeptrack <mail@bleeptrack.de>
2023-11-24 10:28:40 +0100

    Better visualize future bot cards

--- src/Card.svelte
+++ src/Card.svelte
@@ -314,7 +314,7 @@
         align-items: center;
         justify-content: center;
         filter: grayscale(100%);
-        opacity: 0.5;
-        z-index: -1;
+        opacity: 0.3;
+        z-index: 0;
     }
 </style>
